1b00bf0eef89699a30136c39350adf8e2cc43ffb
Diego Guilherme <diegoguilherme752@oultook.com>
2022-10-31 16:12:00 -0300

    feat: redirect button event

--- src/scripts/showMenu.js
+++ src/scripts/showMenu.js
@@ -1,25 +1,28 @@
 function eventShowMenu() {
     const btnMenu = document.querySelector(".button-menu")
-    const formHomePage = document.querySelector(".form-homepage")
+    const formHomePage = document.querySelector(".form-header")
 
     let img = btnMenu.children[0]
 
     btnMenu.addEventListener("click", () => {
         if (btnMenu.id == "show") {
+            formHomePage.classList.toggle("form-show")
             formHomePage.style.animation = "0.7s showMenu ease"
-            formHomePage.style.display = "flex"
+            
             setTimeout(() => {
                 img.src = "/src/assets/img/Vector (4).png"
                 btnMenu.id = "close"
             }, 500)
+
         } else if(btnMenu.id == "close") {
             formHomePage.style.animation = "0.7s closeMenu ease"
+
             setTimeout(() => {
-                formHomePage.style.display = "none"
+                formHomePage.classList.toggle("form-show")
                 img.src = "/src/assets/img/Vector.png"
                 btnMenu.id = "show"
                 formHomePage.style.animation = "none"
-            },600)
+            },400)
         }
     })
 }
