7793c64fcebe900eb908478569c1ca4dd3543994
Diego Guilherme <diegoguilherme752@oultook.com>
2022-11-04 04:18:38 -0300

    feat: render select admim page

--- src/pages/DashBordAdmin/index.js
+++ src/pages/DashBordAdmin/index.js
@@ -1,5 +1,8 @@
 import { eventLogout } from "../../scripts/eventButtons.js";
+import { renderSelectCompany } from "../../scripts/render.js";
 import eventShowMenu from "../../scripts/showMenu.js";
 
 eventShowMenu()
 eventLogout()
+
+renderSelectCompany()
