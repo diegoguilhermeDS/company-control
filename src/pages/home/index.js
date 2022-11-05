5d8be6c21e1c207a7f75f8efb3623ca2f620404a
Diego Guilherme <diegoguilherme752@oultook.com>
2022-11-05 12:04:06 -0300

    feat: message error

--- src/pages/home/index.js
+++ src/pages/home/index.js
@@ -1,7 +1,7 @@
 import { renderSectorOption } from "../../scripts/render.js";
 import { eventShowMenu } from "../../scripts/showMenu.js";
+import { tooltip } from "../../scripts/tooltip.js";
 
 eventShowMenu()
 
 renderSectorOption()
-
